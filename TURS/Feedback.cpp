1b0326905ce5cd724b5d4d7a993e18b36304ac1a
OsasChia <83024499+OsasChia@users.noreply.github.com>
2023-05-23 15:35:06 +0800

    feedback

--- TURS/Feedback.cpp
+++ TURS/Feedback.cpp
@@ -1,7 +1,6 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-// #include "TURS.h"
 using namespace std;
 
 struct Feedback {
@@ -19,121 +18,186 @@ class FeedbackList {
 	Feedback* tail = NULL;
 
 	public:
-	// Constructor
-	FeedbackList();
-
 	// Methods
-	Feedback* CreateNewNode(string feedbackID, string custEmail, string universityID, string feedback, string reply);
+	Feedback* CreateNewNode(string feedbackID, string custEmail, string universityID, string feedback, string reply) {
+		// create a empty new node first
+		Feedback* newnode = new Feedback;
+		newnode->feedbackID = feedbackID;
+		newnode->custEmail = custEmail;
+		newnode->universityID = universityID;
+		newnode->feedback = feedback;
+		newnode->reply = reply;
+		newnode->nextAddress = NULL;
 
-	void InsertToEndList(string feedbackID, string custEmail, string universityID, string feedback, string reply);
+		// newnode address
+		return newnode;
+	}
 
-	// additional feature
-	void DisplayAllFeedbackInfo(); // Big O - O(n)
+	void InsertToEndList(string feedbackID, string custEmail, string universityID, string feedback, string reply) {
+		// call the create function to build a new single node first
+		Feedback* newnode = CreateNewNode(feedbackID, custEmail, universityID, feedback, reply);
 
-	// additional feature
-	void ReplylFeedback(); // Big O - O(n)
-};
-
-
-FeedbackList::FeedbackList() {}
-
-// Methods
-Feedback*
-FeedbackList::CreateNewNode(string feedbackID, string custEmail, string universityID, string feedback, string reply) {
-	// create a empty new node first
-	Feedback* newnode = new Feedback;
-	newnode->feedbackID = feedbackID;
-	newnode->custEmail = custEmail;
-	newnode->universityID = universityID;
-	newnode->feedback = feedback;
-	newnode->reply = reply;
-	newnode->nextAddress = NULL;
-
-	// newnode address
-	return newnode;
-}
-
-void FeedbackList::InsertToEndList(
-	string feedbackID, string custEmail, string universityID, string feedback, string reply) {
-	// call the create function to build a new single node first
-	Feedback* newnode = CreateNewNode(feedbackID, custEmail, universityID, feedback, reply);
-
-	// attach your node to the end of the list
-	if (head == NULL) // list is the empty, always the newnode will be first node in list
-	{
-		head = newnode;
-	} else // if not empty list, just bring to the end of the list.
-	{
-		Feedback* current = head; // to help us find the last item in the list
-
-		while (current->nextAddress != NULL) // if not yet last node, move to next point again
+		// attach your node to the end of the list
+		if (head == NULL) // list is the empty, always the newnode will be first node in list
 		{
-			current = current->nextAddress; // Big O - O(n)
+			head = tail = newnode;
+		} else // if not empty list, just bring to the end of the list.
+		{
+			newnode->prevAddress = tail;
+			tail->nextAddress = newnode;
+			tail = newnode;
+		}
+	}
+
+	void DisplayAllPendingFeedbackInfo() {
+		Feedback* current = head;
+
+		bool existPending; 
+
+		while (current != NULL) // means still not the end of the list
+		{
+			if (current->reply == "pending") {
+				cout << "Favourtie ID: " << current->feedbackID << endl;
+				cout << "Customer Email: " << current->custEmail << endl;
+				cout << "University ID: " << current->universityID << endl;
+				cout << "Feedback: " << current->feedback << endl;
+				cout << "Reply: " << current->reply << endl << endl;
+				current = current->nextAddress; // if you forgot this, will become a infinity loop
+			}
+		}
+		cout << "List is ended here! " << endl;
+	}
+
+	void DisplayUniFeedbackInfo(string universityID) {
+		Feedback* current = head;
+
+		while (current != NULL) // means still not the end of the list
+		{
+			if (current->universityID == universityID) {
+				cout << "Favourtie ID: " << current->feedbackID << endl;
+				cout << "Customer Email: " << current->custEmail << endl;
+				cout << "University ID: " << current->universityID << endl;
+				cout << "Feedback: " << current->feedback << endl;
+				cout << "Reply: " << current->reply << endl << endl;
+				current = current->nextAddress; // if you forgot this, will become a infinity loop
+			}
+		}
+		cout << "List is ended here! " << endl;
+	}
+
+	void DisplaySingleFeedbackInfo(string feedbackID) {
+		Feedback* current = head;
+
+		while (current != NULL) // means still not the end of the list
+		{
+			if (current->feedbackID == feedbackID) {
+				cout << "Favourtie ID: " << current->feedbackID << endl;
+				cout << "Customer Email: " << current->custEmail << endl;
+				cout << "University ID: " << current->universityID << endl;
+				cout << "Feedback: " << current->feedback << endl;
+				cout << "Reply: " << current->reply << endl << endl;
+				current = current->nextAddress; // if you forgot this, will become a infinity loop
+			}
+		}
+		cout << "List is ended here! " << endl;
+	}
+
+	FeedbackList importFeedback() {
+		FeedbackList feedbackList;
+		string file_feedbackID;
+		string file_custEmail;
+		string file_universityID;
+		string file_feedback;
+		string file_reply;
+
+		ifstream file("Feedback.csv");
+		// skip the first line
+		string str;
+		getline(file, str);
+		str.clear();
+		while (file.good()) {
+			getline(file, file_feedbackID, ',');
+			getline(file, file_custEmail, ',');
+			getline(file, file_universityID, ',');
+			getline(file, file_feedback, ',');
+			getline(file, file_reply);
+			if (file_feedbackID == "feedbackID") {
+				continue;
+			} else if (file_feedbackID == "") {
+				break;
+			}
+			feedbackList.InsertToEndList(file_feedbackID, file_custEmail, file_universityID, file_feedback, file_reply);
+		}
+		return feedbackList;
+	}
+
+	void exportFeedback(FeedbackList feedbackList) {
+		ofstream ExportFeedbackFile;
+		ExportFeedbackFile.open("Feedback.csv");
+		Feedback* head = feedbackList.head;
+		if (head == NULL) {
+			return;
+		}
+		ExportFeedbackFile << "feedbackID,custEmail,universityID,feedback,reply" << endl;
+
+		Feedback* current = head;
+
+		while (current != NULL) {
+			ExportFeedbackFile << current->feedbackID << "," << current->custEmail << "," << current->universityID << ","
+												 << current->feedback << "," << current->reply << endl;
+
+			current = current->nextAddress;
+		}
+		ExportFeedbackFile.close();
+	}
+
+	void addFeedback(string custEmail,string universityID,string feedback) {
+		string feedbackID;
+
+		FeedbackList feedbackList = importFeedback();
+
+		int tail = stoi(feedbackList.tail->feedbackID);
+
+		feedbackID = to_string(tail + 1);
+
+		feedbackList.InsertToEndList(feedbackID, custEmail, universityID, feedback, "pending");
+		exportFeedback(feedbackList);
+	}
+
+	void ReplyFeedback(string feedbackID, string reply) {
+		FeedbackList feedbackList = importFeedback();
+
+		if (feedbackList.head == NULL) {
+			return;
 		}
 
-		// if found the last node, attach the new node after the last node
-		current->nextAddress = newnode;
+		Feedback* current = feedbackList.head;
+
+		while (current != NULL) {
+			if (current->feedbackID == feedbackID) {
+				current->reply = reply;
+			}
+			current = current->nextAddress;
+		}
+
+		exportFeedback(feedbackList);
 	}
-}
 
-// additional feature
-void FeedbackList::DisplayAllFeedbackInfo() // Big O - O(n)
-{
-	Feedback* current = head;
+	bool ExistFeedback(string feedbackID) {
+		FeedbackList feedbackList = importFeedback();
 
-	while (current != NULL) // means still not the end of the list
-	{
-		cout << "Favourtie ID: " << current->feedbackID << endl;
-		cout << "Customer Email: " << current->custEmail << endl;
-		cout << "University ID: " << current->universityID << endl;
-		cout << "Feedback: " << current->feedback << endl;
-		cout << "Reply: " << current->reply << endl << endl;
-		current = current->nextAddress; // if you forgot this, will become a infinity loop
+		if (feedbackList.head == NULL) {
+			return false;
+		}
+
+		Feedback* current = feedbackList.head;
+
+		while (current != NULL) {
+			if (current->feedbackID == feedbackID) {
+				return true;
+			}
+			current = current->nextAddress;
+		}
 	}
-	cout << "List is ended here! " << endl;
-}
 
-// additional feature
-void FeedbackList::ReplylFeedback() // Big O - O(n)
-{}
-
-
-// int main()
-//{
-//	//Call object and constructor
-//	FeedbackList feedbackList;
-//	//Define variable
-//	string feedbackID, custEmail, universityID, feedback, reply;
-//	//input file
-//	ifstream file("C://Users//cylin//Desktop//APU//APU2F2209SE//Semester 2//DSTR//Assignment//Feedback.csv");
-//	//skip the first line
-//	string str;
-//	getline(file, str);
-//	str.clear();
-//	while (file.good())
-//	{
-//		getline(file, feedbackID, ',');
-//		getline(file, custEmail, ',');
-//		getline(file, universityID, ',');
-//		getline(file, feedback, ',');
-//		getline(file, reply, '\n');
-//		if (feedbackID == "feedbackID")
-//		{
-//			continue;
-//		}
-//		else if (feedbackID == "")
-//		{
-//			break;
-//		}
-//
-//		feedbackList.InsertToEndList(feedbackID, custEmail, universityID, feedback, reply);
-//	}
-//	file.close();
-//
-//	cout << "Final Feedback List from the CSV file as below: " << endl << string(50, '=') << endl;
-//	feedbackList.DisplayAllFavInfo();
-//	cout << endl << string(50, '=') << endl;
-//
-//
-//	return 0;
-// }
+};
